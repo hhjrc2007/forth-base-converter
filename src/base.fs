1c3cefbb6dd8ee483bb4bb08572474833ed46eb0
foo <hhjrc2007@proton.me>
2026-09-21 19:05:24 +0300

    factor base saving into with-base

--- src/base.fs
+++ src/base.fs
@@ -1,7 +1,10 @@
 : binary ( -- ) 2 base ! ;
 : octal ( -- ) 8 base ! ;
 
-: .base ( n base -- ) base @ >r base ! . r> base ! ;
+: with-base ( n xt base -- ) base @ >r base ! execute r> base ! ;
+
+: .base ( n base -- ) ['] . swap with-base ;
+: u.base ( u base -- ) ['] u. swap with-base ;
 
 : .bin ( n -- ) 2 .base ;
 : .oct ( n -- ) 8 .base ;
