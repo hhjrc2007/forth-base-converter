fe346d6fd9884df62129b7b104fe5ccc921c37b9
foo <hhjrc2007@proton.me>
2026-09-21 18:42:02 +0300

    add .dec/.hex and factor out .base

--- src/base.fs
+++ src/base.fs
@@ -1,4 +1,10 @@
 : binary ( -- ) 2 base ! ;
-: octal ( --) 8 base ! ;
+: octal ( -- ) 8 base ! ;
 
-: .bin ( n -- ) base @ swap binary . base ! ;
+: .base ( n base -- ) base @ >r base ! . r> base ! ;
+
+: .bin ( n -- ) 2 .base ;
+: .oct ( n -- ) 8 .base ;
+: .dec ( n -- ) 10 .base ;
+: .hex ( n -- ) 16 .base ;
+
