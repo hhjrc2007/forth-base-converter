941e787e8ad83b152b790f51cfc951acf8303305
foo <hhjrc2007@proton.me>
2026-09-21 18:55:46 +0300

    report bad input in read-base

--- src/io.fs
+++ src/io.fs
@@ -1,6 +1,7 @@
 include base.fs
 
-: read-num ( "number" -- n ) parse-name s>number? drop drop ;
-: read-base ( base "number" -- n ) base @ >r base ! read-num r> base ! ;
+: read-num ( "number" -- n flag ) parse-name s>number? >r drop r> ;
+: read-base ( base "number" -- n )
+	base @ >r base ! read-num r> base ! 0= abort" not a number" ;
 
 : convert-base ( from to "number" -- ) >r read-base r> .base ;
